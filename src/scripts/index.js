bc599485734d2e58c97a29aebd6bff11a947f1be
Arthur-Adamov <vasiliy.moroz2601@gmail.com>
2024-07-04 23:25:04 +0300

    feat: выполнить открытие и закрытие попапов

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,14 +15,26 @@ const cardContainer = content.querySelector('.places__list')
 function createCard(name, link, deleteCard) {
 
   const card = cardTemplate.querySelector('.places__item').cloneNode(true)
-  const deleteButton = card.querySelector('.card__delete-button')       
+  const deleteButton = card.querySelector('.card__delete-button') 
+  
+  // const popupCardImg = card.querySelector('.popup_type_image')
+  // const cardImg = card.querySelector('.card__image')
+  // console.log(cardImg)
+  // cardImg.addEventListener('click', openCardImg)
 
   card.querySelector('.card__title').textContent = name
   card.querySelector('.card__image').src = link
-  deleteButton.addEventListener('click', deleteCard)    
+  deleteButton.addEventListener('click', deleteCard)
 
   return card
 }
+
+
+// function openCardImg(evt) {
+//   const card = evt.target.closest('.card')
+//   card.classList.add('card__image')
+// }
+
 
 
 // @todo: Функция удаления карточки
@@ -37,4 +49,56 @@ initialCards.forEach( (cardElement) => {
   cardContainer.append(createCard(cardElement.name, cardElement.link, deleteCard))
 })
 
-console.log('Hello!!!')
+
+
+const popupEditProfile = document.querySelector('.popup_type_edit')
+const popupNewCard = document.querySelector('.popup_type_new-card')
+const profileEditBtn = document.querySelector('.profile__edit-button')
+const profileAddBtn = document.querySelector('.profile__add-button')
+// const popupList = document.querySelectorAll('.popup')
+
+
+
+
+
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened')
+
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup__close')) {
+      closePopup(popup)
+    }
+  })
+}
+
+
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened')
+}
+
+profileEditBtn.addEventListener('click', () => {
+  openPopup(popupEditProfile)
+})
+
+profileAddBtn.addEventListener('click', () => {
+  openPopup(popupNewCard)
+})
+
+
+
+// popupCloseBtn.addEventListener('click', (evt) => {
+//   closePopup(evt.target.closest('.popup'))
+//   console.log(evt.target.closest('.popup'))
+// })
+
+// popupCloseBtn.addEventListener('click', () => {
+//   closePopup(popupEditProfile)
+// })
+
+// popupCloseBtn.addEventListener('click', () => {
+//   closePopup(popupCloseBtn.closest('.popup'));
+// })
+
+// popupCloseBtn.addEventListener('click', () => {
+//   console.log('click!!')
+// })
