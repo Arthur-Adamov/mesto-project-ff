609ca15fc8a3b635039fb068873c5f4fdefb87bd
Artur Adamov <artur.adamov03340@yandex.ru>
2024-07-23 12:24:39 +0300

    feat: выполнить 1й шаг Валидации форм

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -56,7 +56,7 @@ profileAddBtn.addEventListener('click', () => {
   openPopup(popupNewCard)
 })
 
-//отправка заполненой формы 
+//отправка заполненой формы
 function handleFormEditProfileSubmit(evt) {
   evt.preventDefault();
 
@@ -67,6 +67,94 @@ function handleFormEditProfileSubmit(evt) {
 }
 
 editProfileForm.addEventListener('submit', handleFormEditProfileSubmit)
+
+//Валидация формы
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+
+  inputElement.classList.add('popup__input_type_error')
+
+  if(!inputElement.value) {
+    errorElement.textContent = 'Вы пропустили это поле.'
+  } else {
+    errorElement.textContent = errorMessage
+  }
+
+  errorElement.classList.add('form__input-error_active')
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+
+  inputElement.classList.remove('popup__input_type_error')
+  errorElement.classList.remove('form__input-error_active')
+  errorElement.textContent = ''
+}
+
+const isValid = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage)
+  } else {
+    inputElement.setCustomValidity("")
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage)
+  } else {
+    hideInputError(formElement, inputElement)
+  }
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if(hasInvalidInput(inputList) ) {
+    buttonElement.disabled = true
+    buttonElement.classList.add('form__submit_inactive')
+  } else {
+    buttonElement.disabled = false
+    buttonElement.classList.remove('form__submit_inactive')
+  }
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  })
+}
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
+  const buttonElement = formElement.querySelector('.popup__button')
+
+
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement)
+
+      toggleButtonState(inputList, buttonElement)
+    })
+  })
+}
+
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'))
+
+  formList.forEach((formElement) => {
+    setEventListeners(formElement)
+  })
+
+}
+
+enableValidation()
+
+
+
+
+
+
+
+
 
 newPlaceForm.addEventListener('submit', (evt) => {
   evt.preventDefault()
